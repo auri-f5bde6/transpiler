2740e492ec5aac7485af459af12c169b84c26ee7
auri <f5bde6@proton.me>
2026-09-26 01:30:09 +0100

    Output tabs of pretty print, ast (wip) and LMC

--- transpiler-wasm/src/lib.rs
+++ transpiler-wasm/src/lib.rs
@@ -3,7 +3,8 @@ use wasm_bindgen::prelude::wasm_bindgen;
 use compiler::compiler::Compiler;
 use compiler::optimiser::Optimiser;
 use parser::parser::Parser;
-use parser::PrettyPrint;
+use parser::{PrettyPrint, Visitor};
+use parser::ast::{AssignStatement, BlockStatement, BooleanLiteral, ExpressionStatement, ForLoopStatement, FunctionCallExpression, Identifier, IfStatement, InfixExpression, IntegerLiteral, PrefixExpression, ProcedureStatement, ReturnStatement, StringLiteral, WhileStatement};
 use type_checker::TypeChecker;
 
 #[wasm_bindgen]
@@ -13,7 +14,7 @@ pub fn transpile(name: &str) -> String {
         Ok(ast) => {
             let errors = TypeChecker::new().check(&ast);
             if let Some(errors) = errors {
-                format!("{:?}", errors)
+                format!("Type error: {:?}", errors)
             } else {
                 let mut result = Compiler::compile(ast);
                 result = Optimiser::new(result).optimise();
@@ -21,7 +22,128 @@ pub fn transpile(name: &str) -> String {
             }
         }
         Err(err) => {
-            format!("Error: {:?}", err)
+            format!("Parser error: {:?}", err)
         }
+    }
+}
+
+#[wasm_bindgen]
+pub fn pretty_print(name: &str) -> String {
+    let mut parser = Parser::from(&*name);
+    match parser.parse() {
+        Ok(ast) => {
+            format!("{}", ast.pretty_print())
+        }
+        Err(err) => {
+            format!("Parser error: {:?}", err)
+        }
+    }
+}
+
+struct Grapher {}
+
+impl Visitor<(), ()> for Grapher {
+    fn visit_identifier(&mut self, ident: &Identifier) -> () {
+        todo!()
+    }
+
+    fn visit_integer(&mut self, literal: &IntegerLiteral) -> () {
+        todo!()
+    }
+
+    fn visit_boolean(&mut self, literal: &BooleanLiteral) -> () {
+        todo!()
+    }
+
+    fn visit_string(&mut self, literal: &StringLiteral) -> () {
+        todo!()
+    }
+
+    fn visit_function_call(&mut self, call: &FunctionCallExpression) -> () {
+        todo!()
+    }
+
+    fn visit_addition(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_subtraction(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_division(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_multiplication(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_equal(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_not_equal(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_greater(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_greater_equal(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_lesser(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_lesser_equal(&mut self, infix: &InfixExpression, left: (), right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_positive(&mut self, prefix: &PrefixExpression, right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_negation(&mut self, prefix: &PrefixExpression, right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_not(&mut self, prefix: &PrefixExpression, right: ()) -> () {
+        todo!()
+    }
+
+    fn visit_assign(&mut self, stmt: &AssignStatement) -> () {
+        todo!()
+    }
+
+    fn visit_return(&mut self, stmt: &ReturnStatement) -> () {
+        todo!()
+    }
+
+    fn visit_expression_statement(&mut self, expr: &ExpressionStatement) -> () {
+        todo!()
+    }
+
+    fn visit_block(&mut self, stmt: &BlockStatement) -> () {
+        todo!()
+    }
+
+    fn visit_if(&mut self, stmt: &IfStatement) -> () {
+        todo!()
+    }
+
+    fn visit_procedure(&mut self, stmt: &ProcedureStatement) -> () {
+        todo!()
+    }
+
+    fn visit_while(&mut self, stmt: &WhileStatement) -> () {
+        todo!()
+    }
+
+    fn visit_for_loop(&mut self, stmt: &ForLoopStatement) -> () {
+        todo!()
     }
 }
--- transpiler-wasm/web/index.ts
+++ transpiler-wasm/web/index.ts
@@ -1,15 +1,21 @@
 import "./styles.css";
 
-import {transpile} from '../pkg';
+import {pretty_print, transpile} from '../pkg';
 
 const code_input = document.getElementById("code")! as HTMLTextAreaElement;
-const output = document.getElementById("output")!;
+const pretty_print_tab_content = document.getElementById("pretty-print-tab-content")!;
+const lmc_tab_content = document.getElementById("lmc-tab-content")!;
 const transpile_button = document.getElementById("floating-arrow")!;
-const slider = document.getElementById("slider") as HTMLDivElement
-const code_half = document.getElementById("code-half-container") as HTMLDivElement;
+const slider = document.getElementById("slider")! as HTMLDivElement
+const code_half = document.getElementById("code-half-container")! as HTMLDivElement;
+const pretty_print_tab = document.getElementById("pretty-print-tab")!;
+const ast_tab = document.getElementById("ast-tab")!;
+const lmc_tab = document.getElementById("lmc-tab")!;
+
 
 function clicked() {
-    output.textContent = transpile(code_input.value);
+    lmc_tab_content.textContent = transpile(code_input.value);
+    pretty_print_tab_content.textContent = pretty_print(code_input.value);
 }
 
 code_input.value = `\
@@ -21,12 +27,12 @@ while number>0
 endwhile
 // 5*4*3*2*1 = 120
 print(result)
-`
+`;
 transpile_button.onclick = clicked;
 
 // https://stackoverflow.com/a/55565128
 slider.onmousedown = function dragMouseDown(e) {
-    e.preventDefault()
+    e.preventDefault();
     let initial = e.clientX;
     let initial_width = code_half.getBoundingClientRect().width;
     document.onmousemove = function onMouseMove(e) {
@@ -34,3 +40,29 @@ slider.onmousedown = function dragMouseDown(e) {
     }
     document.onmouseup = () => document.onmousemove = document.onmouseup = null;
 }
+
+function on_click_apply_selected(elm: HTMLElement) {
+    elm.onclick = () => {
+        reset_selected();
+        elm.classList.add("selected");
+        for_elm_in_class("tab-contents", elm1 => (elm1 as HTMLElement).style.display = "none");
+        const content = document.getElementById(`${elm.id}-content`)!;
+        content.style.display = "block";
+    }
+}
+
+function for_elm_in_class(class_name: string, func: (elm: Element) => void) {
+    const selected = document.getElementsByClassName(class_name);
+    for (let i = 0; i < selected.length; i++) {
+        const element = selected.item(i)!;
+        func(element);
+    }
+}
+
+function reset_selected(): void {
+    for_elm_in_class("selected", elm => elm.classList.remove("selected"));
+}
+
+on_click_apply_selected(pretty_print_tab);
+on_click_apply_selected(ast_tab);
+on_click_apply_selected(lmc_tab);
